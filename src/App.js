34037da25e14aaeddbe37acb31d2cc3cbebebfe0
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-05-21 09:04:25 +0500

    asdas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import QuizForm from './QuizForm';
+import QuizFormResult from './QuizFormResult';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+  	super(props);
+  	this.state = {
+  		result : null
+  	}
+  }
+
+  setResult = (values) =>{
+	  this.setState({
+	  	result : JSON.stringify(values, null, 2)
+	  })
+  }
+
+  render() {
+    return (
+      <div className="App">
+       	<QuizForm onSubmit={this.setResult} />
+       	<QuizFormResult result={this.state.result}/>
+      </div>
+    );
+  }
 }
 
 export default App;
